e9e71f968852c740fda07a561999058012ce51dc
julissaortiz <126351385+julissaortiz@users.noreply.github.com>
2023-08-16 14:04:46 -0500

    updated files

--- client/src/app.jsx
+++ client/src/app.jsx
@@ -1,10 +1,24 @@
-import React from `react`
+import React from 'react'; 
+import { StreamChat } from 'stream-chat';
+import { Chat } from 'stream-chat-react';
+import Cookies from 'universal-cookie';
+
+import ChannelListContainer from './components/ChannelListContainer'; // Remove this line
+import ChannelContainer from './components/ChannelContainer';
+
+const apiKey = 'pb72s7nagt5a';
+
+const client = StreamChat.getInstance(apiKey)
 
 const app = () => {
     return (
-        <div>
-
+        <div className="app__wrapper">
+            <Chat client={client} theme="team light">
+                <ChannelListContainer />
+                <ChannelContainer />
+            </Chat>
         </div>
     );
 }
-export default App
+
+export default app;
